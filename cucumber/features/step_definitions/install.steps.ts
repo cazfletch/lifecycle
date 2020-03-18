31e3d3c7dc9eaeadc273fd8e0520b0e9646fe01c
Caroline Fletcher <cazfletch@gmail.com>
2020-03-18 12:40:46 +0000

    Install smart contract package (#28)
    Added being able to install a smart contract package
    
    closes #8
    
    Signed-off-by: Caroline Fletcher <cazfletch@gmail.com>

--- cucumber/features/step_definitions/install.steps.ts
+++ cucumber/features/step_definitions/install.steps.ts
@@ -11,26 +11,28 @@ import * as sinonChai from 'sinon-chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import {InstallHelper} from '../../helpers/InstallHelper';
 import {Helper} from '../../helpers/Helper';
+import * as path from 'path';
+import {Wallet, Wallets} from 'fabric-network';
 
 const should = chai.should();
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
 When('I install the smart contract', {timeout: 70 * 1000}, async function (): Promise<void> {
-    this.packageId = await InstallHelper.installPackage(this.org1Network, Helper.org1Peer, this.packagePath, this.label, '0.0.1');
-    this.packageId = await InstallHelper.installPackage(this.org2Network, Helper.org2Peer, this.packagePath, this.label, '0.0.1');
+    this.packageId = await InstallHelper.installPackage(this.lifecycle, Helper.org1Peer, this.packagePath, this.wallet, this.org1Identity);
+    this.packageId = await InstallHelper.installPackage(this.lifecycle, Helper.org2Peer, this.packagePath, this.wallet, this.org2Identity);
 });
 
 
 Then(/^the package should be installed on the peer$/, async function (): Promise<void> {
-    const resultOrg1 = await InstallHelper.getInstalledPackages(this.org1Network, Helper.org1Peer);
+    const resultOrg1 = await InstallHelper.getInstalledPackages(this.org1Network, Helper.oldOrg1Peer);
     let exists = resultOrg1.find((data) => {
         return data.label === this.label;
     });
 
     should.exist(exists);
 
-    const resultOrg2 = await InstallHelper.getInstalledPackages(this.org2Network, Helper.org2Peer);
+    const resultOrg2 = await InstallHelper.getInstalledPackages(this.org2Network, Helper.oldOrg2Peer);
     exists = resultOrg2.find((data) => {
         return data.label === this.label;
     });
@@ -39,18 +41,18 @@ Then(/^the package should be installed on the peer$/, async function (): Promise
 });
 
 Given(/^the package is installed$/, {timeout: 70 * 1000}, async function (): Promise<void> {
-    const resultOrg1 = await InstallHelper.getInstalledPackages(this.org1Network, Helper.org1Peer);
+    const resultOrg1 = await InstallHelper.getInstalledPackages(this.org1Network, Helper.oldOrg1Peer);
     let result = resultOrg1.find((data) => data.label === this.label);
     if (!result) {
-        this.packageId = await InstallHelper.installPackage(this.org1Network, Helper.org1Peer, this.packagePath, this.label, '0.0.1');
+        this.packageId = await InstallHelper.installPackage(this.lifecycle, Helper.org1Peer, this.packagePath, this.wallet, this.org1Identity);
     } else {
         this.packageId = result.packageId;
     }
 
-    const resultOrg2 = await InstallHelper.getInstalledPackages(this.org2Network, Helper.org2Peer);
+    const resultOrg2 = await InstallHelper.getInstalledPackages(this.org2Network, Helper.oldOrg2Peer);
     result = resultOrg2.find((data) => data.label === this.label);
     if (!result) {
-        this.packageId = await InstallHelper.installPackage(this.org2Network, Helper.org2Peer, this.packagePath, this.label, '0.0.1');
+        this.packageId = await InstallHelper.installPackage(this.lifecycle, Helper.org2Peer, this.packagePath, this.wallet, this.org2Identity);
     } else {
         this.packageId = result.packageId;
     }
--- test/LifecyclePeer.test.ts
+++ test/LifecyclePeer.test.ts
@@ -4,15 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {LifecyclePeer} from '../src';
-import {Client, Endorser} from 'fabric-common';
+import {Client, Endorsement, Endorser, IdentityContext} from 'fabric-common';
+import * as protos from 'fabric-protos';
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
+import * as chaiAsPromised from 'chai-as-promised';
 import * as path from 'path';
-import {Wallet, Wallets} from 'fabric-network';
+import {Wallet, Wallets, X509Identity} from 'fabric-network';
+import * as sinon from 'sinon';
 
 const should = chai.should();
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
+// tslint:disable:no-unused-expression
 describe('LifecyclePeer', () => {
 
     describe(`constructor`, () => {
@@ -53,6 +58,263 @@ describe('LifecyclePeer', () => {
             should.exist(peer['wallet']);
             // @ts-ignore
             peer['identity'].should.equal('myIdentity');
-        })
-    })
+        });
+    });
+
+    describe('installSmartContractPackage', () => {
+        let mysandbox: sinon.SinonSandbox;
+
+        let peer: LifecyclePeer;
+        let fabricClient: Client;
+        let buildRequest: any;
+        const packageBuffer: Buffer = Buffer.from('mySmartContract');
+        let endorser: Endorser;
+        let wallet: Wallet;
+
+        let endorserConnectStub: sinon.SinonStub;
+        let endorsementBuildSpy: sinon.SinonSpy;
+        let endorsementSignSpy: sinon.SinonSpy;
+        let endorsementSendStub: sinon.SinonStub;
+
+        before(async () => {
+            fabricClient = new Client('myClient');
+            peer = new LifecyclePeer({
+                url: 'grpcs://localhost:7051',
+                mspid: 'myMSPID',
+                name: 'myPeer',
+                pem: '-----BEGIN CERTIFICATE-----\\nMIICJjCCAc2gAwIBAgIURY9F2Rt0JqOtiHbNJ6rRgfiDy2EwCgYIKoZIzj0EAwIw\\ncDELMAkGA1UEBhMCVVMxFzAVBgNVBAgTDk5vcnRoIENhcm9saW5hMQ8wDQYDVQQH\\nEwZEdXJoYW0xGTAXBgNVBAoTEG9yZzEuZXhhbXBsZS5jb20xHDAaBgNVBAMTE2Nh\\nLm9yZzEuZXhhbXBsZS5jb20wHhcNMjAwMzE2MTQ1MDAwWhcNMzUwMzEzMTQ1MDAw\\nWjBwMQswCQYDVQQGEwJVUzEXMBUGA1UECBMOTm9ydGggQ2Fyb2xpbmExDzANBgNV\\nBAcTBkR1cmhhbTEZMBcGA1UEChMQb3JnMS5leGFtcGxlLmNvbTEcMBoGA1UEAxMT\\nY2Eub3JnMS5leGFtcGxlLmNvbTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHic\\nzHXBRqfe7elvQ8zuxIwigOFCuk/49bjChQxf19fL/qHBLYLOXgd3Ox5jTVyyLuO/\\nf9x19piTv7gVgv8h7BijRTBDMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAG\\nAQH/AgEBMB0GA1UdDgQWBBRGw4tXsbZSI45NZNTsDT7rssJpzjAKBggqhkjOPQQD\\nAgNHADBEAiBWNIFkaageeAiEMmhauY3bTHoG45Wgjk99CjHZ6KJoTgIgMfKc9mBL\\na5JHbGNB/gsBhxIm8/akE6g+SikIz/JGty4=\\n-----END CERTIFICATE-----\\n"\n'
+            }, fabricClient);
+
+            wallet = await Wallets.newFileSystemWallet(path.join(__dirname, 'tmp', 'wallet'));
+
+            const peerOrg1Identity: X509Identity = {
+                credentials: {
+                    certificate: '-----BEGIN CERTIFICATE-----\n' +
+                        'MIICujCCAmGgAwIBAgIUUOge6hz++rKSbrV1Ya2t/kcC3s0wCgYIKoZIzj0EAwIw\n' +
+                        'cDELMAkGA1UEBhMCVVMxFzAVBgNVBAgTDk5vcnRoIENhcm9saW5hMQ8wDQYDVQQH\n' +
+                        'EwZEdXJoYW0xGTAXBgNVBAoTEG9yZzEuZXhhbXBsZS5jb20xHDAaBgNVBAMTE2Nh\n' +
+                        'Lm9yZzEuZXhhbXBsZS5jb20wHhcNMjAwMzE3MTU0NzAwWhcNMjEwMzE3MTU1MjAw\n' +
+                        'WjBgMQswCQYDVQQGEwJVUzEXMBUGA1UECBMOTm9ydGggQ2Fyb2xpbmExFDASBgNV\n' +
+                        'BAoTC0h5cGVybGVkZ2VyMQ4wDAYDVQQLEwVhZG1pbjESMBAGA1UEAxMJb3JnMWFk\n' +
+                        'bWluMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEoELj0ASt7TpEAUNJjPkG7zNY\n' +
+                        'wLMP3LDsFc38rWKm6ZRGtJIQ5k7jcoXXScuhS1YuRop/xKAvOhiLbd1hyo7fAqOB\n' +
+                        '6DCB5TAOBgNVHQ8BAf8EBAMCB4AwDAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQUEKQz\n' +
+                        'qJPP/lPsRO7BizDnjJptylswHwYDVR0jBBgwFoAUSS9NU/qBB+Wx0R7PispddLmZ\n' +
+                        'fd0wKAYDVR0RBCEwH4IdQ2Fyb2xpbmVzLU1hY0Jvb2stUHJvLTMubG9jYWwwWwYI\n' +
+                        'KgMEBQYHCAEET3siYXR0cnMiOnsiaGYuQWZmaWxpYXRpb24iOiIiLCJoZi5FbnJv\n' +
+                        'bGxtZW50SUQiOiJvcmcxYWRtaW4iLCJoZi5UeXBlIjoiYWRtaW4ifX0wCgYIKoZI\n' +
+                        'zj0EAwIDRwAwRAIgHhpQYEAtQ9rPhvh4Wer3hKtuq7FChoqJWkj9rNdnbIkCIC4I\n' +
+                        'GzjjS7hZRKsETRvIT3LRFTZJLJq6AcGOtepFso/n\n' +
+                        '-----END CERTIFICATE-----',
+                    privateKey: '-----BEGIN PRIVATE KEY-----\n' +
+                        'MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgvprUzTpK7GBMXVvI\n' +
+                        'NOlGgxyqqi1TM6CA63qNK8PTwfihRANCAASgQuPQBK3tOkQBQ0mM+QbvM1jAsw/c\n' +
+                        'sOwVzfytYqbplEa0khDmTuNyhddJy6FLVi5Gin/EoC86GItt3WHKjt8C\n' +
+                        '-----END PRIVATE KEY-----',
+                },
+                mspId: `myMSPID`,
+                type: 'X.509',
+            };
+
+            await wallet.put('myIdentity', peerOrg1Identity);
+        });
+
+        beforeEach(() => {
+            mysandbox = sinon.createSandbox();
+
+            peer.setCredentials(wallet, 'myIdentity');
+            peer['requestTimeout'] = undefined;
+
+            endorser = fabricClient.getEndorser('myPeer', 'myMSPID');
+            endorserConnectStub = mysandbox.stub(endorser, 'connect').resolves();
+
+            endorsementBuildSpy = mysandbox.spy(Endorsement.prototype, 'build');
+            endorsementSignSpy = mysandbox.spy(Endorsement.prototype, 'sign');
+            endorsementSendStub = mysandbox.stub(Endorsement.prototype, 'send');
+            endorsementSendStub.resolves();
+
+            const arg = new protos.lifecycle.InstallChaincodeArgs();
+            arg.setChaincodeInstallPackage(packageBuffer);
+
+            buildRequest = {
+                fcn: 'InstallChaincode',
+                args: [arg.toBuffer()]
+            };
+        });
+
+        afterEach(() => {
+            mysandbox.restore();
+        });
+
+        it('should install the smart contract package', async () => {
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                responses: [{
+                    response: {
+                        status: 200,
+                        payload: encodedResult
+                    }
+                }]
+            });
+
+            const result: string | undefined = await peer.installSmartContractPackage(packageBuffer);
+            result!.should.equal('myPackageId');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser]
+            });
+        });
+
+        it('should install the smart contract package using the timeout passed in', async () => {
+            peer['requestTimeout'] = 1234;
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                responses: [{
+                    response: {
+                        status: 200,
+                        payload: encodedResult
+                    }
+                }]
+            });
+
+            const result: string | undefined = await peer.installSmartContractPackage(packageBuffer, 4321);
+            result!.should.equal('myPackageId');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser],
+                requestTimeout: 4321
+            })
+        });
+
+        it('should install the smart contract package using the timeout', async () => {
+            peer['requestTimeout'] = 1234;
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                responses: [{
+                    response: {
+                        status: 200,
+                        payload: encodedResult
+                    }
+                }]
+            });
+
+            const result: string | undefined = await peer.installSmartContractPackage(packageBuffer);
+            result!.should.equal('myPackageId');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser],
+                requestTimeout: 1234
+            });
+        });
+
+        it('should handle wallet or identity not being set', async () => {
+            peer['wallet'] = undefined;
+            peer['identity'] = undefined;
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Wallet or identity property not set, call setCredentials first');
+        });
+
+        it('should handle identity not being in the wallet', async () => {
+            peer['identity'] = 'otherIdentity';
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Could not install smart contact received error: Identity otherIdentity does not exist in the wallet');
+        });
+
+        it('should handle errors in the response', async () => {
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                errors: [new Error('some error')]
+            });
+
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Could not install smart contact received error: some error');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser]
+            });
+        });
+
+        it('should handle a non 200 status code', async () => {
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                responses: [{
+                    response: {
+                        status: 400,
+                        message: 'some error'
+                    }
+                }]
+            });
+
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Could not install smart contact received error: Smart contract install failed with status:400 ::some error');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser]
+            });
+        });
+
+        it('should handle no response details', async () => {
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({
+                responses: [{
+                    response: {}
+                }]
+            });
+
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Could not install smart contact received error: Smart contract install has failed');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser]
+            });
+        });
+
+        it('should handle no response', async () => {
+            const encodedResult = protos.lifecycle.InstallChaincodeResult.encode({
+                package_id: 'myPackageId'
+            });
+
+            endorsementSendStub.resolves({});
+
+            await peer.installSmartContractPackage(packageBuffer).should.eventually.be.rejectedWith('Could not install smart contact received error: No response returned for install of smart contract');
+
+            endorserConnectStub.should.have.been.called;
+            endorsementBuildSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext), buildRequest);
+            endorsementSignSpy.should.have.been.calledWith(sinon.match.instanceOf(IdentityContext));
+            endorsementSendStub.should.have.been.calledWith({
+                targets: [endorser]
+            });
+        });
+    });
 });
